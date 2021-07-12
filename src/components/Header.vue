a7a7d332eabd97d519d8fa57135c6e85329ae849
Luca-Riz <77754414+Luca-Riz@users.noreply.github.com>
2021-07-12 12:05:06 +0200

    add skeleton

---
+++ src/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="header">
+    <div class="title">
+      <h1>{{ msg }}</h1>
+    </div>
+
+    <div class="search">
+      <input type="text">
+      <button>Search</button>
+      <button>Reset</button>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <h2>{{ msg }}</h2>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
