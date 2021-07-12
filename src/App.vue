b3622662f74ce8b9454d76f4835ce96002fecd60
Luca-Riz <77754414+Luca-Riz@users.noreply.github.com>
2021-07-12 18:48:13 +0200

    add axios on App

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,56 @@
 <template>
   <div id="app">
     
-    <Header msg="Boolflix" @inputText="apiMod" />
-    <Main msg="This is the main"/>
+    <Header msg="Boolflix"  /> 
+    <!-- @inputText="apiMod" andrà dentro qui sopra -->
+    <Main v-for="(film,index) in filmList" :key="index"
+      :title="film.title"
+      :originalTitle="film.original_title"
+      :language="film.original_language"
+      :vote="film.vote_average"/>   
+    
 
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 import Header from './components/Header.vue'
 import Main from './components/Main.vue'
 
 export default {
   name: 'App',
+
+  data(){
+    return {
+      apiURL: 'https://api.themoviedb.org/3/search/movie?api_key=e14a682f2cb51ebef668a83973649087&query=avatar&language=it-IT',
+      filmList: []
+    }
+  },
+
   components: {
     Header,
     Main
   },
 
-  methods: {
-    apiMod(){
-      // da fare 
-    }
+  created(){
+    this.getFilm();
   },
 
+
+
+
+    methods: {
+      getFilm(){
+        axios
+          .get(this.apiURL)
+          .then(response => {
+            // console.log(response.data);
+            this.filmList = response.data.results;
+            console.log(this.filmList);
+          })
+      }
+    },
 
 }
 </script>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,12 +2,12 @@
   <div class="hello">
     <h2>{{ msg }}</h2>
 
-    <!-- estrapolo i dati dall'array filmList -->
-    <ul v-for="(film,index) in filmList" :key="index">
-      <strong>Film: {{ film.title }}</strong> 
-      <li >Titolo originale: {{ film.original_title }}</li>
-      <li >Lingua originale: {{ film.original_language }}</li>
-      <li >Voto medio: {{ film.vote_average }}</li>
+
+    <ul>
+      <li>Film: {{ title }}</li> 
+      <li >Titolo originale: {{ originalTitle }}</li>
+      <li >Lingua originale: {{ language }}</li>
+      <li >Voto medio: {{ vote }}</li>
     </ul>
 
     
@@ -15,45 +15,27 @@
 </template>
 
 <script>
-import axios from 'axios';
+
 export default {
   name: 'Main',
   props: {
-    msg: String
+    msg: String,
+    title: String,
+    originalTitle: String,
+    language: String,
+    vote: Number,
 
   },
 
  
 
-  data() {
-      return {
-          apiURL : 'https://api.themoviedb.org/3/search/movie?api_key=e14a682f2cb51ebef668a83973649087&query=avatar&language=it-IT',
-          filmList : [],
-          loading: true,
-          searchText: ''
-      }
-  },
 
-  created(){
-    this.getFilm();
-  },
 
-  methods: {
-    getFilm(){
-      axios
-        .get(this.apiURL)
-        .then(response => {
-          // console.log(response.data);
-          this.filmList = response.data.results;
-          console.log(this.filmList);
-        })
-    }
-  }
+
   
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 
 </style>
