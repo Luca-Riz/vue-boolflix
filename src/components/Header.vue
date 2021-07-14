a56878713edfe2010d3548e901f79cba78473f5a
Luca-Riz <77754414+Luca-Riz@users.noreply.github.com>
2021-07-14 15:55:24 +0200

    add header graphic

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="header">
-    <div class="title">
-      <h1>{{ msg }}</h1>
+  <div class="header d-flex justify-content-between align-items-center">
+    <div class="title m-3">
+      <h1 class="text-uppercase">{{ msg }}</h1>
     </div>
 
-    <div class="search">
-      <input 
+    <div class="search m-3 d-flex">
+
+      <input class="form-control"
       type="text"
       placeholder="Digita cosa cercare"
       v-model.trim="inputText">
 
-      <button
+      <button class="btn btn-dark"
       type="submit"
-      @click="setApi">Search</button>
+      @click="setApi">
+      Search
+      </button>
 
     </div>
 
@@ -44,5 +47,8 @@ export default {
 
 <style scoped lang="scss">
 
+h1{
+  color:red;
+}
   
 </style>
